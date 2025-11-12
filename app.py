6be1917c4f2185b510603cc0fe6c907c629b4232
Parag Sharma <paragsharma602@gmail.com>
2025-11-12 23:37:39 +0530

    new changes
    Set the port for the Flask app to use an environment variable or default to 10000.

--- app.py
+++ app.py
@@ -87,4 +87,6 @@ def krishimitra_api():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
